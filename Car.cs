636248ea979fed843ba459bf69480620580e1b99
NikitaEvgenievih <nikitabykov2006hag@gmail.com>
2022-11-11 19:21:39 +0200

    Events

---
+++ Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11dz
+{
+    public class Car
+    {
+        public string Company { get; set; }
+        public string Model { get; set; }
+        public double Speed { get; set; }
+
+        public event StartCarDelegate? StartCar;
+        public event DriveCarDelegate? DriveCar;
+
+        public Car(string company, string model, double speed)
+        {
+            Company = company;
+            Model = model;
+            Speed = speed;
+        }
+
+        public void Start()
+        {
+            StartCar?.Invoke(this, new EventArgs());
+        }
+        public void Drive()
+        {
+            DriveCar?.Invoke(this, new EventArgs());
+        }
+
+        public delegate void StartCarDelegate(object auto_1, EventArgs e_1);
+        public delegate void DriveCarDelegate(object auto_2, EventArgs e_2);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp11dz;
+
+//Почему то из-за DriveCar все ломается!!!(Вродебы все ок, не могу понять ?почему?)
+
+Car car_1 = new Car("BMW", "X5", 100);
+car_1.StartCar += StartAuto;
+car_1.Start();
+
+Console.WriteLine("------------------------------------------");
+
+Car car_2 = new Car("Toyota", "Camry", 90);
+car_2.DriveCar += DriveAuto;
+car_2.Start();
+
+Console.WriteLine("------------------------------------------");
+
+Car car_3 = new Car("Mercedes", "C-Class", 120);
+car_3.StartCar += StartAuto;
+car_3.DriveCar += DriveAuto;
+car_3.Start();
+
+void StartAuto(object auto_1, EventArgs e_1)
+{
+    if (auto_1 is Car car)
+    {
+        Console.WriteLine($"Заведена машина {car.Company } {car.Model}");
+    }
+}
+
+void DriveAuto(object auto_2, EventArgs e_2)
+{
+    if (auto_2 is Car car)
+    {
+        Console.WriteLine($"Стартовала машина {car.Company } {car.Model} со скоростью {car.Speed} км/час");
+    }
+}
+
